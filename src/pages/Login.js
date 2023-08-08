a0af92dce4ab84b58ffb5e7c04d946edbfcb9d98
Kouyoumjian <adrien.kouyoumjian@outlook.fr>
2023-08-08 15:26:13 +0200

    login worked

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,30 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from "react-router-dom";
+import Axios from "axios";
+import React, { useState } from "react";
 
 export const Login = () => {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async () => {
+    try {
+      const response = await Axios.post(
+        "http://localhost:8000/api/login_check",
+        {
+          email: email,
+          password: password,
+        }
+      );
+
+      // const token = response.data.token; 
+      // console.log("JWT Token:", token);
+      navigate('/dashboard');
+    } catch (error) {
+      console.error("Erreur de connexion :", error);
+    }
+  };
+
   return (
     <div className="login-page">
       <div className="login-part-one">
@@ -16,14 +40,37 @@ export const Login = () => {
       <div className="login-part-two">
         <div className="login-space">
           <h1>Connexion</h1>
-          <input type="text" placeholder="E-MAIL" />
-          <input type="password" placeholder="MOT DE PASSE" />
-          <p>Vous n'avez pas de compte ? <Link to="/register" className="register-link">S'inscrire</Link></p>
-          <div className='center-btn'>
-            <button>Se connecter</button>
-          </div>  
+          <input
+            type="text"
+            placeholder="E-MAIL"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="MOT DE PASSE"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <p>
+            Vous n'avez pas de compte ?{" "}
+            <Link to="/register" className="register-link">
+              S'inscrire
+            </Link>
+          </p>
+          <div className="center-btn">
+            <button onClick={handleLogin}>Se connecter</button>
+          </div>
         </div>
       </div>
     </div>
   );
 };
+
+
+/*inscription 
+inscrit dans db 
+token renvoyé dans la requete 
+tester le email
+déconnexion automatique avec le token 
+*/
