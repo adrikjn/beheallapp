8fe4583e3817d1a165e54557e29eeb18ee439cb9
Kouyoumjian <adrien.kouyoumjian@outlook.fr>
2023-08-08 12:57:13 +0200

    push

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -9,17 +9,20 @@ export const Register = () => {
           <img src="/facture.png" alt="facture" />
         </div>
       </div>
-        <h1 className="">Inscription</h1>
+        <h1 className="register-title">Inscription</h1>
       <div className="register">
         <div className="name-inputs">
           <input type="text" placeholder="Prenom" />
           <input type="text" placeholder="Nom" />
         </div>
-        <input type="text" placeholder="Nom de la société" />
         <input type="email" placeholder="E-mail" />
+        <input type="number" placeholder="Numéro de téléphone" />
         <input type="password" placeholder="Mot de passe" />
         <input type="password" placeholder="Confirmation du mot de passe" />
       </div>
+      <div className='align-btn'>
+            <button>S'inscrire</button>
+          </div>  
     </div>
   );
 };
