61c69cf6a1fdbca92f246b83c4ad30c5dbf4986f
Kouyoumjian <adrien.kouyoumjian@outlook.fr>
2023-08-25 12:45:11 +0200

    push

--- src/pages/InvoiceStepThree.js
+++ src/pages/InvoiceStepThree.js
@@ -3,7 +3,19 @@ import React from "react";
 export const InvoiceStepThree = () => {
   return (
     <div className="invoice-step-one-page">
-        yo
+      <div className="welcome-user">
+        <h1>creation factures</h1>
+        <img src="/profil-icon.svg" alt="facture" />
+      </div>
+      <div className="invoice-step-one-title">
+        <h2>Etape</h2>
+        <h2>N°3</h2>
+      </div>
+      <div className="invoice-create">
+        <p>yo</p>
+        <p>a</p>
+        <p>b</p>
+      </div>
     </div>
   );
 };
