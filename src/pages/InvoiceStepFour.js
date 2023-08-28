c179eea2a405b5524c234f5a23d07f6d74a52dbb
Kouyoumjian <adrien.kouyoumjian@outlook.fr>
2023-08-28 16:27:26 +0200

    push

--- src/pages/InvoiceStepFour.js
+++ src/pages/InvoiceStepFour.js
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import React, {useEffect } from "react";
+import React, { useEffect } from "react";
 
 export const InvoiceStepFour = () => {
   const token = localStorage.getItem("Token");
@@ -11,7 +11,6 @@ export const InvoiceStepFour = () => {
     }
   }, [token, navigate]);
 
-
   return (
     <div className="invoice-step-one-page">
       <div className="welcome-user">
@@ -22,7 +21,27 @@ export const InvoiceStepFour = () => {
         <h2>Etape</h2>
         <h2>N°4</h2>
       </div>
-    
+      <div className="add-product">
+        lalal
+        lalal
+      </div>
+      <div className="product-list">
+        <ul className="product-header">
+          <li>Produits</li>
+          <li>Quantité</li>
+          <li>TVA</li>
+          <li>Prix HT</li>
+        </ul>
+        <ul className="product-item">
+          <li>Test</li>
+          <li>Test</li>
+          <li>Test</li>
+          <li>Test</li>
+        </ul>
+        <div className="center-plus">
+          <img src="/plus.svg" alt="facture" />
+        </div>
+      </div>
     </div>
   );
 };
