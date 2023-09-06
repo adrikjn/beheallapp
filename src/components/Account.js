31cd99f0197f1893c397b012609bc36a82ce510a
[Adrien Kouyoumjian] <[adrien.kouyoumjian@outlook.fr]>
2023-09-06 17:43:19 +0200

    push

--- src/components/Account.js
+++ src/components/Account.js
@@ -1,8 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Account = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const navigate = useNavigate();
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleLogout = () => {
+    // Supprimer les données stockées localement
+    localStorage.removeItem("Token");
+    localStorage.removeItem("invoice");
+    localStorage.removeItem("UserData");
+    localStorage.removeItem("InvoiceData");
+
+    // Utiliser navigate pour rediriger l'utilisateur vers la page de connexion
+    navigate("/login");
+  };
+
   return (
-    <img src="/profil-icon.svg" alt="account" />
+    <div className="account-container">
+      <img
+        src="/profil-icon.svg"
+        alt="account"
+        className="account-icon"
+        onClick={toggleMenu}
+      />
+      {isOpen && (
+        <div className="account-menu">
+          <a href="/mon-profil">Profil</a>
+          <button onClick={handleLogout}>Déconnexion</button>
+        </div>
+      )}
+    </div>
   );
 };
 
