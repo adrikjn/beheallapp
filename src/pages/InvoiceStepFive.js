e0bf46b2a70fcb916f1df5d296ab88eeb85ee4b2
[Adrien Kouyoumjian] <[adrien.kouyoumjian@outlook.fr]>
2023-09-11 18:05:55 +0200

    push

--- src/pages/InvoiceStepFive.js
+++ src/pages/InvoiceStepFive.js
@@ -2,6 +2,8 @@ import { useNavigate } from "react-router-dom";
 import React, { useEffect, useState } from "react";
 import AccordionNav from "../components/AccordionNav";
 import Account from "../components/Account";
+import jsPDF from 'jspdf';
+import 'jspdf-autotable';
 
 export const InvoiceStepFive = () => {
   const token = localStorage.getItem("Token");
@@ -86,6 +88,102 @@ export const InvoiceStepFive = () => {
     }
   };
 
+  const generateInvoicePDF = () => {
+    const pdf = new jsPDF();
+    pdf.setFont("helvetica");
+    pdf.setFontSize(12);
+    pdf.setTextColor(0, 0, 0); // Couleur du texte : Noir
+  
+    // En-tête de la facture
+    pdf.setFontSize(18);
+    pdf.text(20, 20, "Facture");
+  
+    // Informations sur l'expéditeur
+    pdf.setFontSize(14);
+    pdf.text(20, 40, "Expéditeur");
+    pdf.setFontSize(12);
+    pdf.text(20, 50, `Nom: ${userData?.firstName?.toUpperCase()} ${userData?.lastName?.toUpperCase()}`);
+    pdf.text(20, 60, `Nom: ${invoiceData?.company?.name}`);
+    pdf.text(20, 70, `Adresse: ${invoiceData?.company?.address}, ${invoiceData?.company?.postalCode}, ${invoiceData?.company?.city}`);
+    pdf.text(20, 80, `Email: ${invoiceData?.company?.email}`);
+    pdf.text(20, 90, `Téléphone: ${invoiceData?.company?.phoneNumber}`);
+  
+    // Informations sur le destinataire
+    pdf.setFontSize(14);
+    pdf.text(100, 40, "Destinataire");
+    pdf.setFontSize(12);
+    pdf.text(100, 50, `Nom: ${invoiceData?.customer?.companyName}`);
+    pdf.text(100, 60, `Adresse: ${invoiceData?.customer?.address}, ${invoiceData?.customer?.postalCode}, ${invoiceData?.customer?.city}`);
+    pdf.text(100, 70, `Email: ${invoiceData?.customer?.email}`);
+    pdf.text(100, 80, `Téléphone: ${invoiceData?.customer?.phoneNumber}`);
+  
+    // Liste des produits
+    pdf.setFontSize(16);
+  pdf.text(20, 110, "Liste des produits");
+
+  
+    // Tableau pour afficher les produits
+    const tableStyle = {
+      theme: "grid",
+      tableWidth: "auto", // Ajustez la largeur de la table en conséquence
+      styles: {
+        font: "helvetica",
+        fontSize: 10,
+        textColor: [0, 0, 0], // Couleur du texte : Noir
+        cellPadding: 5,
+        overflow: "linebreak",
+      },
+      headStyles: {
+        fillColor: [238, 238, 238], // Couleur de fond pour les en-têtes
+        textColor: [0, 0, 0], // Couleur du texte pour les en-têtes
+        fontStyle: "bold",
+      },
+    };
+  
+    // Tableau pour afficher les produits en utilisant jsPDF-AutoTable
+    const productsTable = {
+      headers: ["Intitulés", "Volumes", "Tarif", "TVA", "Prix HT"],
+      rows: [],
+    };
+  
+    invoiceData?.services?.forEach((service) => {
+      productsTable.rows.push([
+        service.title,
+        service.quantity,
+        `${service.unitCost}€`,
+        `${service.vat}%`,
+        `${service.totalPrice}€`,
+      ]);
+    });
+  
+    // Dessiner le tableau
+    pdf.autoTable({
+      startY: 120, // Ajustez la position Y en conséquence
+      head: [productsTable.headers],
+      body: productsTable.rows,
+      ...tableStyle, // Appliquez le style CSS défini ci-dessus
+    });
+  
+    // Montant total TTC
+    const totalAmount = `Montant total TTC: ${invoiceData?.totalPrice.toFixed(2)}€`;
+    pdf.setFontSize(14);
+    pdf.text(20, pdf.autoTable.previous.finalY + 20, totalAmount);
+  
+    // Conditions générales de vente
+    pdf.setFontSize(12);
+    pdf.text(20, pdf.autoTable.previous.finalY + 40, `Conditions générales de vente: ${invoiceData?.company?.gcs}`);
+  
+    // Télécharger le PDF avec un nom de fichier personnalisé
+    const fileName = `${invoiceData?.company?.name.replace(/\s+/g, "_").toUpperCase()}_Facture_${invoiceData?.billNumber.toUpperCase()}_${formatDate(invoiceData?.createdAt)}.pdf`;
+    pdf.save(fileName);
+  };
+  
+  // generateInvoicePDF();
+
+  
+  
+  
+
   return (
     <div className="invoice-step-one-page">
       <div className="welcome-user">
@@ -93,6 +191,8 @@ export const InvoiceStepFive = () => {
         <Account />
       </div>
       <div className="summary">
+      <button onClick={generateInvoicePDF}>Télécharger Facture PDF</button>
+        
         <div className="company-summary">
           <h2>Expéditaire</h2>
           <div className="company-summary-part">
@@ -183,6 +283,7 @@ export const InvoiceStepFive = () => {
             <p>Moyen de paiement : {invoiceData?.paymentMethod}</p>
             <p>Conditions générales de vente : {invoiceData?.company?.gcs}</p>
           </div>
+
           <div className="btn-invoice-2">
             <button onClick={sendInvoice}>Envoyer</button>
           </div>
