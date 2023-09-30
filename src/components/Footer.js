6facbbc022ec8dc1905b3e09c83ded6669ad0577
[Adrien Kouyoumjian] <[adrien.kouyoumjian@outlook.fr]>
2023-09-30 21:33:56 +0200

    x

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom"; // Importez Link depuis react-router-dom
+import { Link } from "react-router-dom"; 
 
 const Footer = () => (
   <div className="footer">
