b63c227ce8277213d21ce19f0aee4bb9bfa1239d
[Adrien Kouyoumjian] <[adrien.kouyoumjian@outlook.fr]>
2023-11-03 11:14:24 +0100

    x

--- src/pages/ResetPassword.js
+++ src/pages/ResetPassword.js
@@ -7,8 +7,8 @@ import Footer from "../components/Footer.js";
 
 export const ResetPassword = () => {
   const hasToken = !!localStorage.getItem("Token");
-
   const [email, setEmail] = useState("");
+  const [showOverlay, setShowOverlay] = useState(false);
 
   const handleEmailChange = (e) => {
     setEmail(e.target.value);
@@ -17,11 +17,20 @@ export const ResetPassword = () => {
   const handleSendEmail = async (e) => {
     e.preventDefault();
     try {
-    const response = await axios.post("https://api.beheall.com/reset-password", { email });
+      await axios.post("https://api.beheall.com/reset-password", { email });
+      setShowOverlay(true);
+      setEmail("");
       console.log("E-mail de réinitialisation envoyé avec succès !");
     } catch (error) {
-      console.error("Erreur lors de l'envoi de l'e-mail de réinitialisation :", error);
+      console.error(
+        "Erreur lors de l'envoi de l'e-mail de réinitialisation :",
+        error
+      );
     }
+  };
+
+  const closeOverlay = () => {
+    setShowOverlay(false);
   };
 
   return (
@@ -33,26 +42,38 @@ export const ResetPassword = () => {
           content="Besoin d'accéder à votre compte Beheall mais avez oublié votre mot de passe ? Réinitialisez-le facilement et continuez à créer vos factures gratuitement avec Beheall."
         />
       </Helmet>
-       <LogoAndPicture />
-       <div className="login-border"></div>
-    <div className="reset-password-div">
-       <h1>Réinitialisation du mot de passe</h1>
-      <form onSubmit={handleSendEmail}>
-        <input
-          type="email"
-          placeholder="E-mail"
-          value={email}
-          onChange={handleEmailChange}
-        />
-        <div className="align-btn">
-          <button type="submit">Envoyer</button>
+      <LogoAndPicture />
+      <div className="login-border"></div>
+      <div className="reset-password-div">
+        <h1>Réinitialisation du mot de passe</h1>
+        <form onSubmit={handleSendEmail}>
+          <input
+            type="email"
+            placeholder="E-mail"
+            value={email}
+            onChange={handleEmailChange}
+          />
+          <div className="align-btn">
+            <button type="submit">Envoyer</button>
+          </div>
+        </form>
+      </div>
+      {showOverlay && (
+        <div className="overlay-password">
+          <div className="overlay-content">
+            <p>
+              Si l'adresse e-mail est associée à Beheall, les instructions de
+              réinitialisation de votre mot de passe seront envoyées.
+            </p>
+            <div className="pw-reset-right-btn">
+               <button onClick={closeOverlay}>Fermer</button>
+            </div>
+           
+          </div>
         </div>
-        
-      </form>
-    </div>
-    <Footer />
+      )}
+      <Footer />
       {hasToken && <AccordionNav />}
-     
     </div>
   );
 };
