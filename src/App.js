588822ec939e477983a61db94bdeeb4f9866bb02
[Adrien Kouyoumjian] <[adrien.kouyoumjian@outlook.fr]>
2023-11-21 21:00:14 +0100

    x

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useEffect } from 'react';
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { LandingPage } from "./pages/LandingPage";
