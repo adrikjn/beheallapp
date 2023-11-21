eac3a70ad432fadc679072d6f6988f874ab5e832
[Adrien Kouyoumjian] <[adrien.kouyoumjian@outlook.fr]>
2023-11-21 14:20:19 +0100

    cx

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -50,9 +50,6 @@ export const Login = () => {
 
       localStorage.setItem("Token", response.data.token);
 
-      const decodedToken = jwtDecode(response.data.token);
-      console.log("Decoded Token:", decodedToken);
-
       navigate("/dashboard");
     } catch (error) {
       console.error("Erreur de connexion :", error);
