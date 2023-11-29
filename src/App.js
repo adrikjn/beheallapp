09dd0ecf2476da2fe921933f19662d4ea38570a5
[Adrien Kouyoumjian] <[adrien.kouyoumjian@outlook.fr]>
2023-11-29 14:17:43 +0100

    x

--- src/App.js
+++ src/App.js
@@ -41,18 +41,22 @@ function App() {
   
     // Action à exécuter avant le déchargement de la page
     const handleBeforeUnload = () => {
-      // Stocker le token dans le sessionStorage
-      sessionStorage.setItem("Token", token);
+      // Supprimer le token du localStorage lors de la fermeture de la page
+      localStorage.removeItem("Token");
     };
   
     // Ajouter l'événement beforeunload
     window.addEventListener("beforeunload", handleBeforeUnload);
+  
+    // Stocker le token dans le localStorage lors du rafraîchissement de la page
+    localStorage.setItem("Token", token);
   
     // Nettoyer l'événement lors du démontage du composant
     return () => {
       window.removeEventListener("beforeunload", handleBeforeUnload);
     };
   }, []);
+  
   return (
     <div className="App">
       <Router>
