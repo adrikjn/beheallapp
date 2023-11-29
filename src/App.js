9c4f10130189539343b6f5b8c39fb6fd6765da20
[Adrien Kouyoumjian] <[adrien.kouyoumjian@outlook.fr]>
2023-11-29 17:33:31 +0100

    x

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { LandingPage } from "./pages/LandingPage";
 import { LegalNotice } from "./pages/LegalNotice";
@@ -15,60 +15,10 @@ import { InvoiceStepTwo } from "./pages/InvoiceStepTwo";
 import { InvoiceStepThree } from "./pages/InvoiceStepThree.js";
 import { InvoiceStepFour } from "./pages/InvoiceStepFour.js";
 import { InvoiceStepFive } from "./pages/InvoiceStepFive.js";
+import useAuthEffect from "./utils/useAuthEffect";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(!!localStorage.getItem("Token"));
-
-  useEffect(() => {
-    // Fonction pour déconnecter l'utilisateur
-    const logoutUser = () => {
-      localStorage.clear();
-      setIsLoggedIn(false);
-    };
-
-    // Définir une temporisation de 15 minutes après la connexion
-    const timeoutId = setTimeout(logoutUser, 15 * 60 * 1000); // 3 minutes en millisecondes
-
-    // Nettoyer le timeout lors du démontage du composant ou lorsqu'il y a une déconnexion
-    return () => {
-      clearTimeout(timeoutId);
-    };
-  }, [isLoggedIn]);
-
-  useEffect(() => {
-    // Fonction pour gérer le déchargement de la page
-    const handleBeforeUnload = () => {
-      // Stocker le timestamp actuel dans le localStorage
-      localStorage.setItem("logoutTime", new Date().getTime());
-    };
-
-    // Ajouter l'événement beforeunload
-    window.addEventListener("beforeunload", handleBeforeUnload);
-
-    // Nettoyer l'événement lors du démontage du composant
-    return () => {
-      window.removeEventListener("beforeunload", handleBeforeUnload);
-    };
-  }, []);
-
-  useEffect(() => {
-    // Récupérer le timestamp de déconnexion du localStorage
-    const storedLogoutTime = localStorage.getItem("logoutTime");
-
-    // Vérifier si le timestamp existe et si le délai de 15 minutes n'a pas été dépassé
-    if (
-      storedLogoutTime &&
-      new Date().getTime() - parseInt(storedLogoutTime) < 15 * 60 * 1000
-    ) {
-      // Annuler la suppression du token
-      return;
-    }
-
-    // Supprimer le token après 1 (4 millisecondes) seconde
-    setTimeout(() => {
-      localStorage.removeItem("Token");
-    }, 1);
-  }, []);
+  useAuthEffect();
 
   return (
     <div className="App">
---
+++ src/utils/useAuthEffect.js
@@ -0,0 +1,60 @@
+import { useState, useEffect } from "react";
+
+function useAuthEffect() {
+  const [isLoggedIn, setIsLoggedIn] = useState(!!localStorage.getItem("Token"));
+
+  useEffect(() => {
+    // Fonction pour déconnecter l'utilisateur
+    const logoutUser = () => {
+      localStorage.clear();
+      setIsLoggedIn(false);
+    };
+
+    // Définir une temporisation de 15 minutes après la connexion
+    const timeoutId = setTimeout(logoutUser, 15 * 60 * 1000); // 15 minutes en millisecondes
+
+    // Nettoyer le timeout lors du démontage du composant ou lorsqu'il y a une déconnexion
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [isLoggedIn]);
+
+  useEffect(() => {
+    // Fonction pour gérer le déchargement de la page
+    const handleBeforeUnload = () => {
+      // Stocker le timestamp actuel dans le localStorage
+      localStorage.setItem("logoutTime", new Date().getTime());
+    };
+
+    // Ajouter l'événement beforeunload
+    window.addEventListener("beforeunload", handleBeforeUnload);
+
+    // Nettoyer l'événement lors du démontage du composant
+    return () => {
+      window.removeEventListener("beforeunload", handleBeforeUnload);
+    };
+  }, []);
+
+  useEffect(() => {
+    // Récupérer le timestamp de déconnexion du localStorage
+    const storedLogoutTime = localStorage.getItem("logoutTime");
+
+    // Vérifier si le timestamp existe et si le délai de 15 minutes n'a pas été dépassé
+    if (
+      storedLogoutTime &&
+      new Date().getTime() - parseInt(storedLogoutTime) < 15 * 60 * 1000
+    ) {
+      // Annuler la suppression du token
+      return;
+    }
+
+    // Supprimer le token après 1 (4 millisecondes) seconde
+    setTimeout(() => {
+      localStorage.removeItem("Token");
+    }, 1);
+  }, []);
+
+  return isLoggedIn;
+}
+
+export default useAuthEffect;
